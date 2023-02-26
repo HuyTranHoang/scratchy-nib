1a6b376fc6025835ebd48ddb8e0c66d707ad0f26
Tran Hoang Huy <nhokpio95@gmail.com>
2023-02-26 16:51:08 +0700

    Component footer, refactor a bit

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="text-center text-lg-start bg-secondary-color text-muted text-primary-color">
-    <div class="container text-center text-md-start mt-5">
-        <div class="tab-content accordion row mt-3 bg-secondary-color" id="myTabContent">
-            <div class="col-md-4 col-lg-4 col-xl-3 mx-auto">
+    <div class="container text-center text-md-start">
+        <div class="tab-content accordion row mt-3 bg-secondary-color" id="footerContent">
+            <div class="col-md-4 col-lg-4 col-xl-3 mx-auto pt-3">
                 <h4 class="text-uppercase fw-bold navbar-title text-center footer-text-color">
                     Important
                 </h4>
@@ -10,82 +10,55 @@
                     You may NOT use any of my projects to create items for sale without a&nbsp;<a class="text-decoration-none" href="#">commercial license</a>.
                 </p>
                 <ul class="footer-icon">
-                    <li><a href="#" class="me-4 text-reset">
-                            <i class="fab fa-facebook-f"></i></a>
-                    </li>
-                    <li><a href="#" class="me-4 text-reset">
-                            <i class="fab fa-twitter"></i></a>
-                    </li>
-                    <li><a href="#" class="me-4 text-reset">
-                            <i class="fab fa-google"></i></a>
-                    </li>
-                    <li><a href="#" class="me-4 text-reset">
-                            <i class="fab fa-instagram"></i></a>
-                    </li>
-                    <li><a href="#" class="me-4 text-reset">
-                            <i class="fab fa-github"></i></a>
-                    </li>
+                    <li><a href="#">
+                            <i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#">
+                            <i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#">
+                            <i class="fab fa-google"></i></a></li>
+                    <li><a href="#">
+                            <i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#">
+                            <i class="fab fa-github"></i></a></li>
                 </ul>
             </div>
-            <div class="col-md-4 col-lg-2 col-xl-2 mx-auto tab-pane active text-uppercase footer-text-color" id="sitemap-tab-pane" role="tabpanel" aria-labelledby="sitemap-tab" tabindex="0">
-                <h2 class="accordion-header d-lg-none">
-                    <button class="accordion-button border-bottom collapsed bg-secondary-color footer-text-color" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        Site Map
-                    </button>
-                </h2>
-                <div id="collapseTwo" class="accordion-collapse collapse d-lg-block" data-bs-parent="#myTabContent" >
-                    <div class="accordion-body" id="myTabContent">
-                        <h6 class="text-uppercase fw-bold text-center footer-text-color">
-                            Site Map
-                        </h6>
-                            <p class="border-bottom border-white">
-                                <a href="/about" class="text-reset text-decoration-none">
-                                    <i class="fa-light fa-arrow-right"></i>
-                                    About</a>
-                            </p>
-                            <p class="border-bottom border-white">
-                                <a href="/contact" class="text-reset text-decoration-none">
-                                    <i class="fa-light fa-arrow-right"></i>
-                                    Contact Us</a>
-                            </p>
-                            <p class="border-bottom border-white">
-                                <a href="#" class="text-reset text-decoration-none">
-                                    <i class="fa-light fa-arrow-right"></i>
-                                    FAQ</a>
-                            </p>
-                            <p class="border-bottom border-white">
-                                <a href="#" class="text-reset text-decoration-none">
-                                    <i class="fa-light fa-arrow-right"></i>
-                                    License</a>
-                            </p>
-                            <p class="border-bottom border-white">
-                                <a href="#" class="text-reset text-decoration-none">
-                                    <i class="fa-light fa-arrow-right"></i>
-                                    Privacy Policy</a>
-                            </p>
-                        </div>
-                </div>
-            </div>
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto tab-pane active mb-md-0" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">
-                <h2 class="accordion-header d-lg-none">
-                    <button class="accordion-button border-bottom collapsed bg-secondary-color footer-text-color" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Contact Us
-                    </button>
-                </h2>
-                <div id="collapseThree" class="accordion-collapse collapse d-lg-block" data-bs-parent="#myTabContent" >
-                    <div class="accordion-body">
-                        <h6 class="text-uppercase fw-bold text-center footer-text-color">
-                            Contact
-                        </h6>
-                        <p class="border-bottom border-white"><i class="fas fa-home me-3 footer-text-color "></i>HO CHI MINH, ZIP 70000, VN</p>
-                        <p class="border-bottom border-white"><i class="fas fa-envelope me-3 footer-text-color"></i>scratchynib@gmail.com</p>
-                        <p class="border-bottom border-white"><i class="fas fa-phone me-3 footer-text-color"></i>+84 999 999 99</p>
-                        <p class="border-bottom border-white"><i class="fas fa-print me-3 footer-text-color "></i>+84 888 888 88</p>
-                    </div>
-                </div>
-            </div>
+
+            <x-partials.footer-accordion-list title="Navigate" id="footer_navigate">
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="/about" class="link-secondary text-decoration-none">
+                            About</a>
+                    </li>
+                    <li>
+                        <a href="/contact" class="link-secondary text-decoration-none">
+                            Contact Us</a>
+                    </li>
+                    <li>
+                        <a href="#" class="link-secondary text-decoration-none">
+                            FAQ</a>
+                    </li>
+                    <li>
+                        <a href="#" class="link-secondary text-decoration-none">
+                            License</a>
+                    </li>
+                    <li>
+                        <a href="#" class="link-secondary text-decoration-none">
+                            Privacy Policy</a>
+                    </li>
+                </ul>
+            </x-partials.footer-accordion-list>
+
+            <x-partials.footer-accordion-list title="Contact" id="footer_contact">
+                <ul class="list-unstyled">
+                    <li class="text-primary-color">HO CHI MINH, ZIP 70000, VN</li>
+                    <li><a class="link-secondary" href = "mailto: scratchynib@gmail.com">scratchynib@gmail.com</a></li>
+                    <li><a class="link-secondary" href="tel:+8499999999">+84 999 999 99</a></li>
+                    <li><a class="link-secondary" href="tel:+8488888888">+84 888 888 88</a></li>
+                </ul>
+            </x-partials.footer-accordion-list>
+
             <div class="text-center p-4">
-                ©2023
+                Sitemap | ©2023 Scratchy Nib | All rights reserved.
             </div>
         </div>
     </div>
