15968b01c06bd0194e1314a344ef6c2b603febe0
Huynh Bao <huynhbao0626@gmail.com>
2023-02-27 21:07:18 +0700

    reponsive footer

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="text-center text-lg-start bg-secondary-color text-muted text-primary-color">
     <div class="container text-center text-md-start">
         <div class="tab-content accordion row mt-3 bg-secondary-color" id="footerContent">
-            <div class="col-md-4 col-lg-4 col-xl-3 mx-auto pt-3">
+            <div class="col-md-4 col-lg-4 pt-3">
                 <h4 class="text-uppercas footer-text-color">
                     Content Disclaimer
                 </h4>
@@ -23,7 +23,7 @@
                 </ul>
             </div>
 
-            <x-partials.footer-accordion-list title="Navigate" id="footer_navigate">
+            <x-partials.footer-accordion-list title="Navigate" id="footer_navigate" class="col-md-4 col-lg-4">
                 <ul class="list-unstyled">
                     <li>
                         <a href="/about" class="link-secondary text-decoration-none">
@@ -48,12 +48,12 @@
                 </ul>
             </x-partials.footer-accordion-list>
 
-            <x-partials.footer-accordion-list title="Contact" id="footer_contact">
+            <x-partials.footer-accordion-list title="Contact" id="footer_contact" class="col-md-4 col-lg-4">
                 <ul class="list-unstyled">
                     <li class="text-primary-color">HO CHI MINH, ZIP 70000, VN</li>
-                    <li><a class="link-secondary" href = "mailto: scratchynib@gmail.com">scratchynib@gmail.com</a></li>
-                    <li><a class="link-secondary" href="tel:+8499999999">+84 999 999 99</a></li>
-                    <li><a class="link-secondary" href="tel:+8488888888">+84 888 888 88</a></li>
+                    <li><a class="link-secondary text-decoration-none" href = "mailto: scratchynib@gmail.com">scratchynib@gmail.com</a></li>
+                    <li><a class="link-secondary text-decoration-none" href="tel:+8499999999">+84 999 999 99</a></li>
+                    <li><a class="link-secondary text-decoration-none" href="tel:+8488888888">+84 888 888 88</a></li>
                 </ul>
             </x-partials.footer-accordion-list>
 
