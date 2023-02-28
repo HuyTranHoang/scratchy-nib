2b6d14544d68a0b844843f47dec63c0e2230c208
Huynh Bao <huynhbao0626@gmail.com>
2023-02-28 09:33:09 +0700

    demo Gallery

--- resources/views/users/gallery.blade.php
+++ resources/views/users/gallery.blade.php
@@ -1,4 +1,77 @@
 <x-layouts.master>
-    <h1 class="text-center text-primary">Gallery</h1>
-    <img src="https://i.kym-cdn.com/photos/images/original/002/348/324/517.jpg" alt="Cuti Gura">
+    <header class="bg-secondary-color">
+        <x-partials.overlay-bottom />
+    </header>
+    <section>
+        <div class="container">
+            <div class="row-cols-lg-6 d-flex justify-content-end mb-2">
+                <form action="" class="d-flex form-outline">
+                    <input class="form-control" id="" name="" type="text" placeholder="Search" aria-label="search">
+                    <button class="btn btn-primary-color" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="row">
+                <div class="col-2 bg-secondary-color">
+                    <div class="p-2 text-center">
+                        <h2 class="text-secondary mb-4">Browse by Categories</h2>
+                            <div class="list-group bg-secondary-color" id="list-tab" role="tablist">
+                                <a class="list-group-item list-group-item-action border-bottom border-0" id="list-home-list" data-toggle="list" href="">India</a>
+                                <a class="list-group-item list-group-item-action border-bottom border-0" id="list-home-list" data-toggle="list" href="">Vietnam</a>
+                                <a class="list-group-item list-group-item-action border-bottom border-0" id="list-home-list" data-toggle="list" href="">China</a>
+                            </div>
+                    </div>
+                </div>
+                <div class="col-10">
+                    <h3 class="bg-secondary-color">Gallery</h3>
+                    <div class="row text-center text-lg-start ">
+                        <div class="col-lg-3 col-md-4 col-6 mb-2 ">
+                            <div class="card bg-secondary-color">
+                                <img class="card-img-top" src="https://i.kym-cdn.com/photos/images/original/002/348/324/517.jpg" alt="Error">
+                                <div class="card-body">
+                                    <h5 class="card-title">Name</h5>
+                                    <p class="card-text">Decription</p>
+                                    <p>Date</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-6 mb-2 ">
+                            <div class="card bg-secondary-color">
+                                <img class="card-img-top" src="https://i.kym-cdn.com/photos/images/original/002/348/324/517.jpg" alt="Error">
+                                <div class="card-body">
+                                    <h5 class="card-title">Name</h5>
+                                    <p class="card-text">Decription</p>
+                                    <p>Date</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-6 mb-2 ">
+                            <div class="card bg-secondary-color">
+                                <img class="card-img-top" src="https://i.kym-cdn.com/photos/images/original/002/348/324/517.jpg" alt="Error">
+                                <div class="card-body">
+                                    <h5 class="card-title">Name</h5>
+                                    <p class="card-text">Decription</p>
+                                    <p>Date</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-6 mb-2 ">
+                            <div class="card bg-secondary-color">
+                                <img class="card-img-top" src="https://i.kym-cdn.com/photos/images/original/002/348/324/517.jpg" alt="Error">
+                                <div class="card-body">
+                                    <h5 class="card-title">Name</h5>
+                                    <p class="card-text">Decription</p>
+                                    <p>Date</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="bg-secondary-color">
+        <x-partials.overlay-top />
+    </footer>
 </x-layouts.master>
