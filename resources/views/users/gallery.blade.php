f83b9484c84fc0d565a3152a8e5534ec1b7a7522
Huynh Bao <huynhbao0626@gmail.com>
2023-03-03 04:10:36 +0700

    css hover gallery

--- resources/views/users/gallery.blade.php
+++ resources/views/users/gallery.blade.php
@@ -3,7 +3,7 @@
         <x-users.partials.overlay-bottom />
     </header>
     <section>
-        <div class="container">
+        <div class="container ">
             <div class="row mb-2 rounded">
                 <div class="col-lg-6 col-md-6 col-sm-8 d-lg-flex d-md-flex d-flex mb-3">
                     <select class="form-select rounded-pill" aria-label="SelectClass">
@@ -24,7 +24,7 @@
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-4 d-lg-flex d-md-flex justify-content-lg-end justify-content-md-end mb-3">
                     <form action="" class="d-flex form-outline">
-                        <input class="form-control rounded-pill rounded-end" id="" name="" type="text" placeholder="Search" aria-label="search">
+                        <input class="form-control  rounded-pill rounded-end" id="" name="" type="text" placeholder="Search" aria-label="search">
                         <button class="btn rounded-pill rounded-start btn-primary-color" type="submit">
                             <i class="fas fa-search"></i>
                         </button>
