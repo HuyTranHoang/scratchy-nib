9de397c15badaf55f6fffbb4c8b9a35fb31485d6
Tran Hoang Huy <nhokpio95@gmail.com>
2023-03-02 09:48:08 +0700

    Fix CSS gallery

--- resources/views/users/gallery.blade.php
+++ resources/views/users/gallery.blade.php
@@ -2,93 +2,6 @@
     <header class="bg-secondary-color">
         <x-users.partials.overlay-bottom />
     </header>
-    <style>
-        .card-gallery{
-            position: relative;
-            overflow: hidden;
-        }
-        .card-body-gallery{
-            width: 100%;
-            height: 100%;
-            bottom: -100%;
-            right: 0;
-            position: absolute;
-            background-color: #1f3d4738;
-            backdrop-filter: blur(5px);
-            border-radius: 5px;
-            padding: 5px;
-            color: whitesmoke;
-            flex-direction: column;
-            justify-content: center;
-            transition: .3s;
-        }
-
-        .card-gallery:hover .card-body-gallery{
-            bottom: 0;
-        }
-        .card-title-gallery{
-            text-transform: uppercase;
-            font-size: 1rem;
-            font-weight: 500;
-            margin-bottom: 0;
-            position: absolute;
-            top: 65%;
-
-        }
-        .card-category{
-            text-transform: capitalize;
-            font-size: 0.7rem;
-            font-weight: 300;
-            position: absolute;
-            top: 75%;
-        }
-        .card-btn{
-            background-color: oldlace;
-            color: #1f3d47;
-            padding: 1px 5px;
-            border-radius: 5px;
-            text-transform: capitalize;
-            border: none;
-            outline: none;
-            font-weight: 500;
-            position: absolute;
-            top: 85%;
-            text-decoration: none;
-        }
-
-        /* clear fix */
-        .grid:after {
-            content: '';
-            display: block;
-            clear: both;
-        }
-
-        /* ---- .grid-item ---- */
-
-        .grid-sizer,
-        .grid-item {
-            width: calc(25% - 10px);
-            margin-top: 10px;
-        }
-        .grid-item {
-            float: left;
-        }
-
-        .grid-item img {
-            display: block;
-            max-width: 100%;
-        }
-        .grid-item img:hover {
-        }
-        @media (max-width: 767px) {
-            .grid-item {
-                width: calc(100% - 10px);
-            }
-        }
-        .lb-data .lb-close {
-            display: none;
-        }
-    </style>
     <section>
         <div class="container">
             <div class="row mb-2 rounded">
@@ -120,11 +33,11 @@
                 <div class="grid-sizer"></div>
                 <div class="grid-item">
                     <div class="card-gallery">
-                        <img class="card-img-gallery " src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg"  alt="Error"/>
+                        <img class="card-img-gallery" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg"  alt="Error"/>
                         <a class="card-body-gallery" href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg" data-lightbox="gallery">
                             <h6 class="card-title-gallery">Name</h6>
-                            <p class="card-category">Categories</p>
-                            <a class="card-btn" href="/about">Detial</a>
+                            <p class="card-category">Category</p>
+                            <a class="card-btn text-decoration-none" href="/about">Detail</a>
                         </a>
                     </div>
                 </div>
