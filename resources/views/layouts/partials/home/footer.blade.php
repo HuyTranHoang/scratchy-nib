d229ad1bce228dfa718e3f0b32f7a73ef87916b3
Huy.th <nhokpio95@gmail.com>
2023-03-17 05:58:33 +0700

    Update footer.blade.php

--- resources/views/layouts/partials/home/footer.blade.php
+++ resources/views/layouts/partials/home/footer.blade.php
@@ -55,7 +55,7 @@
                     <li><a class="link-secondary text-decoration-none" href="tel:+8499999999">+84 999 999 99</a></li>
                     <li><a class="link-secondary text-decoration-none" href="tel:+8488888888">+84 888 888 88</a></li>
                     <li><hr></li>
-                    <li style="font-size: 0.8rem">Visitors counter: {{$visitorCount}} </li>
+                    <li style="font-size: 0.8rem">Visitors counter: {{ number_format($visitorCount) }} </li>
                 </ul>
             </x-home.footer-accordion-list>
 
